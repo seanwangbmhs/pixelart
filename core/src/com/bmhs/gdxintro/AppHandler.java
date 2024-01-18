46abe4b0cdac92e374bd9b0411fa46c751648fa2
CIN33R <badams@bmhs-la.org>
2024-01-18 13:29:48 -0800

    first commit

--- core/src/com/bmhs/gdxintro/AppHandler.java
+++ core/src/com/bmhs/gdxintro/AppHandler.java
@@ -5,18 +5,28 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.bmhs.gdxintro.gfx.assets.Tile;
+import com.bmhs.gdxintro.gfx.utils.TileHandler;
 
 public class AppHandler extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+
+	//delete me
+	//Texture img;
+	//TextureRegion subImg;
 
 	int x, y;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
+		//delete me
+		//img = new Texture("primaryColorSheet.png");
+		//subImg = new TextureRegion(TileHandler.getTileHandler().getWorldTileArray().get(0).getTexture(), 64, 0, 128, 128);
 
 		x = 0;
 		y = 0;
@@ -27,7 +37,16 @@ public class AppHandler extends ApplicationAdapter {
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
 
-		batch.draw(img, x, y);
+		for(int r = 0; r < Gdx.graphics.getHeight(); r+=64) {
+			for(int c = 0; c < Gdx.graphics.getWidth(); c+=64) {
+				batch.draw(TileHandler.getTileHandler().getWorldTileArray().get(x++).getTexture(), c, r);
+				if(x > 4) {
+					x = 0;
+				}
+			}
+		}
+
+
 
 		batch.end();
 
@@ -55,22 +74,22 @@ public class AppHandler extends ApplicationAdapter {
 
 		x = Gdx.input.getX() - img.getWidth()/2;
 		y = Gdx.graphics.getHeight() - Gdx.input.getY() - img.getHeight()/2;
-		*/
+
 
 
 		if(x < (Gdx.input.getX() - img.getWidth()/2)) {
-			x+=20;
+			x += 20;
 		}
 		if(x > (Gdx.input.getX() - img.getWidth()/2)) {
-			x-=20;
+			x -= 20;
 		}
 		if(y > (Gdx.graphics.getHeight() - Gdx.input.getY() - img.getHeight()/2)) {
-			y-=20;
+			y -= 20;
 		}
 		if(y < (Gdx.graphics.getHeight() - Gdx.input.getY() - img.getHeight()/2)) {
-			y+=20;
+			y += 20;
 		}
-
+*/
 
 
 
@@ -79,6 +98,5 @@ public class AppHandler extends ApplicationAdapter {
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
